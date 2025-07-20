08ac6e97cb7bacb43a340d768548959c2d8bc9b9
Ryan Phan <ryanphan11@gmail.com>
2025-07-19 17:21:40 -0700

    Initial Commit

---
+++ WhatIsFootBall/WhatIsFootBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsFootBallApp.swift
+//  WhatIsFootBall
+//
+//  Created by Ryan Phan on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsFootBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
